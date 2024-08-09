a208120e30b1c487acd2447605b3c2404c69c692
ValentinKravtsov <kravtsov_vyu@mail.ru>
2024-08-09 18:03:44 +0300

    Переменные

---
+++ 1-07.py
@@ -0,0 +1,6 @@
+number_complete_tasks = 12
+number_hours_tasks = 1.5
+course_name = 'Python'
+time_1_task = number_hours_tasks / number_complete_tasks
+print('Курс: ', course_name, ', всего задач: ', number_complete_tasks, ', затрачено часов: ', number_hours_tasks,
+      ', среднее время выполнения ', time_1_task, '.')
