aec48af56e13e11819e3c79713780ac5f6b35fdb
ValentinKravtsov <kravtsov_vyu@mail.ru>
2024-09-16 18:00:41 +0300

    house

--- module_5/house_module_5_1.py
+++ module_5/house_module_5_1.py
@@ -5,7 +5,7 @@ class House:
 
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors:
-            print("Такого этажа не существует")
+            print(f"Этажа № {new_floor} в '{self.name}' не существует, так как в нём только {self.number_of_floors} этажей")
         else:
             i = 1
             while i <= new_floor:
