eee08d43bce155f396cfb4086ef40b1a5efa1da3
valentin <kravtsov.vyu@gmail.com>
2024-11-05 15:12:21 +0300

    Банковские операции

---
+++ module_10/bank_operation_module_10_3.py
@@ -0,0 +1,46 @@
+import threading, random
+from time import sleep
+
+class Bank:
+
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for i in range(100):
+            count = random.randint(50, 500)
+            self.balance += count
+            print(f'Пополнение: {count}. Баланс: {self.balance}')
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            count = random.randint(50, 500)
+            print(f'Запрос на: {count}')
+            if count <= self.balance:
+                self.balance -= count
+                print(f'Снятие: {count}. Баланс: {self.balance}')
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                self.lock.acquire()
+            sleep(0.001)
+
+
+
+bk = Bank()
+
+# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
+
+
