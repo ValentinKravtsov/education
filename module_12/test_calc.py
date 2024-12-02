716d925ca90701f1db31df96275e92f0a7c89977
valentin <kravtsov.vyu@gmail.com>
2024-12-02 13:48:20 +0300

    тест калькулятора

--- module_12/test_calc.py
+++ module_12/test_calc.py
@@ -3,12 +3,31 @@ import unittest
 
 class CalcTest(unittest.TestCase):
     def test_add(self):
+        """
+        Test for add function in calculator
+        :return:
+        """
         self.assertEqual(calc.add(5, 20), 25)
+
     def test_sub(self):
+        """
+        Test for sub function in calculator
+        :return:
+        """
         self.assertEqual(calc.sub(45, 10), 35)
+
     def test_mul(self):
+        """
+        Test for mul function in calculator
+        :return:
+        """
         self.assertEqual(calc.mul(5, 20), 100)
+
     def test_div(self):
+        """
+        Test for div function in calculator
+        :return:
+        """
         self.assertEqual(calc.div(20, 5), 4)
 
 
