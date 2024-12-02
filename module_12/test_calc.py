364255d43af54e48b60773e9c146e9cfd68c1e11
valentin <kravtsov.vyu@gmail.com>
2024-12-02 13:14:23 +0300

    тест калькулятора

--- module_12/test_calc.py
+++ module_12/test_calc.py
@@ -1,35 +1,16 @@
 import calc
+import unittest
+
+class CalcTest(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.add(5, 20), 25)
+    def test_sub(self):
+        self.assertEqual(calc.sub(45, 10), 35)
+    def test_mul(self):
+        self.assertEqual(calc.mul(5, 20), 100)
+    def test_div(self):
+        self.assertEqual(calc.div(20, 5), 4)
 
 
-def test_add():
-    if calc.add(5, 7) == 12:
-        print('test ADD is OK')
-    else:
-        print('test ADD is FAIL')
-
-
-def test_sub():
-    if calc.sub(13, 9) == 4:
-        print('test SUB is OK')
-    else:
-        print('test SUB is FAIL')
-
-
-def test_mul():
-    if calc.mul(5, 8) == 40:
-        print('test MUL is OK')
-    else:
-        print('test MUL is FAIL')
-
-
-def test_div():
-    if calc.div(90, 10) == 9:
-        print('test DIV is OK')
-    else:
-        print('test DIV is FAIL')
-
-
-test_add()
-test_sub()
-test_mul()
-test_div()
+if __name__ == '__main__':
+    unittest.main()
