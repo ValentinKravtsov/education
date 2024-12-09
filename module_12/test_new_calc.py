a4d8d1b3ad08a66c2f50c487f61ff26a1bff1c70
valentin <kravtsov.vyu@gmail.com>
2024-12-09 15:19:42 +0300

    калькулятор

--- module_12/test_new_calc.py
+++ module_12/test_new_calc.py
@@ -2,9 +2,11 @@ import calc
 import unittest
 
 class NewCalcTest(unittest.TestCase):
+    @unittest.skip("не используется")
     def test_sqrt(self):
         self.assertEqual(calc.sqrt(4), 2)
 
+    @unittest.skipIf(8 > 2, 'Не повезло')
     def test_pow(self):
         self.assertEqual(calc.pow(8, 5), 32768)
 
