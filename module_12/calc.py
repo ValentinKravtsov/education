e6fc7f45f84c140ad673c8bc3374b40d060cc08d
valentin <kravtsov.vyu@gmail.com>
2025-01-09 19:57:04 +0300

    калькулятор

--- module_12/calc.py
+++ module_12/calc.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def add(a, b):
     return a + b
 
@@ -11,14 +14,30 @@ def mul(a, b):
 
 
 def div(a, b):
-    return a / b
+    try:
+        a / b
+        logging.info(f"Successful divide {a} / {b}")
+        return a / b
+    except ZeroDivisionError as err:
+        logging.error('No divide zero', exc_info=True)
+        return 0
 
 def sqrt(a):
-    return a**0.5
+    if a > 0:
+        logging.info(f"Successful sqrt {a}")
+        return a**0.5
+    else:
+        logging.error("number must be >= 0")
+        return 0
 
 def pow(a, b):
     return a**b
 
 
 if __name__ == '__main__':
-    pass
+    logging.basicConfig(level=logging.INFO, filemode="w", filename="py.log",
+                        format="%(asctime)s | %(levelname)s | %(message)s")
+    # print(div(3, 4))
+    # print(div(3, 0))
+    print(sqrt(4))
+    print(sqrt(-4))
